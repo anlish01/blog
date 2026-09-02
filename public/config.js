650599833939e54331f6200b3aafacf3f5820117
kejiland <kejiland@azhz.eu.org>
2026-09-02 09:41:43 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -65,11 +65,11 @@ window.BLOG_CONFIG = {
     text: '',
     icp: '',
     contact: [
-      // { text: 'GitHub', url: 'https://github.com/yourname' },
-      // { text: '邮箱', url: 'mailto:you@example.com' }
+      { text: 'Docs', url: 'https://docs.2024921.xyz/' },
+      { text: 'GitHub', url: 'https://github.com/kejiland/qingyu-blog' }
     ],
     links: [
-     { text: '雨幕', url: 'https://www.yumus.cn' }
+     { text: '语幕', url: 'https://www.yumus.cn' }
     ],
     decl: '本站部分内容转载自网络，作品版权归原作者及来源网站所有，任何内容转载、商业用途等均须联系原作者并注明来源。',
     email: 'admin@cloumail.com',
